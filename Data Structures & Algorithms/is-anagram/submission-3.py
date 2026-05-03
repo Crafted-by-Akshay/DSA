161fb990dc0f2f665ff8222ae7925a9edac5c2a7
Crafted-by-Akshay <65214919+Crafted-by-Akshay@users.noreply.github.com>
2026-05-03 22:48:29 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,23 @@
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if not s and not t:
+            return True
+
+        if not s or not t or len(s)!= len(t):
+            return False
+
+        s_map = Counter(s)
+
+        for c in t:
+            if c not in s_map:
+                return False
+            
+            s_map[c] -=1
+            
+            if s_map[c] == 0:
+                del s_map[c]
+
+        return len(s_map) == 0 
+        
