576586b2cca97cf57c2937544871e976ba2d3e94
Crafted-by-Akshay <65214919+Crafted-by-Akshay@users.noreply.github.com>
2026-05-03 22:51:16 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,31 @@
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if not s and not t:
+            return True
+
+        if len(s)!= len(t):
+            return False
+
+        s_map = Counter(s)
+
+        for c in t:
+            if c not in s_map:
+                return False
+            
+            s_map[c] -=1
+            
+            if s_map[c] == 0:
+                del s_map[c]
+
+        return len(s_map) == 0 
+        
+
+# Time complexity:
+#------------------
+# O(t+s) ~ O(2n) ~ O(n)
+
+# Space complexity:
+#------------------
+# O(t) ~ O(n)
